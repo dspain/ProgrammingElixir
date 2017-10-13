370655ac8877a3ebf8cd1cba957d77ec2d9ad6c8
dspain <dmspain@yahoo.com>
2017-10-12 22:05:00 -0700

    generic location for the weather function

--- Chapter7/weather.exs
+++ Chapter7/weather.exs
@@ -1,9 +1,9 @@
 defmodule WeatherHistory do
-  def for_location_27([]), do: []
-  def for_location_27([ [ time, 27, temp, rain ] | tail ]) do
-    [ [time, 27, temp, rain] | for_location_27(tail) ]
+  def for_location([], _target_loc), do: []
+  def for_location([ [ time, target_loc, temp, rain ] | tail ], target_loc) do
+    [ [time, 27, temp, rain] | for_location(tail, target_loc) ]
   end
-  def for_location_27([ _ | tail]), do: for_location_27(tail)
+  def for_location([ _ | tail], target_loc), do: for_location(tail, target_loc)
 
   #################
   def test_data do
@@ -17,7 +17,7 @@ defmodule WeatherHistory do
       [1366232822, 26, 22, 0.095],
       [1366232822, 27, 21, 0.05],
       [1366232822, 28, 24, 0.03],
-      [1366236422, 26, 17, 0.025],      
+      [1366236422, 26, 17, 0.025],
     ]
   end
 end
