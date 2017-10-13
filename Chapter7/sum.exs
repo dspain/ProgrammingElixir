da533ecd2bbafaf3a094e18bed2f81cfd0348270
dspain <dmspain@yahoo.com>
2017-10-12 21:27:23 -0700

    privatize methods

--- Chapter7/sum.exs
+++ Chapter7/sum.exs
@@ -2,6 +2,6 @@ defmodule MyList do
   def sum(list), do: _sum(list, 0)
 
   # private methods
-  def _sum([], total), do: total
-  def _sum([head|tail],total), do: _sum(tail, head + total)
+  defp _sum([], total), do: total
+  defp _sum([head|tail],total), do: _sum(tail, head + total)
 end
