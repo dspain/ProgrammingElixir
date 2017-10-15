e2f3c95fdf82804c638cfbd9921dc0aa3528b4ba
dspain <dmspain@yahoo.com>
2017-10-14 21:01:30 -0700

    ListsAndRecursion-5 my versions of Enum module functions - 'each'

--- Chapter10/my_enum.exs
+++ Chapter10/my_enum.exs
@@ -6,4 +6,12 @@ defmodule MyEnum do
     func.(head) && all?(tail, func)
   end
 
+  def each([], _func) do
+    :ok
+  end
+  def each([head|tail], func) do
+    func.(head)
+    each(tail, func)
+  end
+
 end
