5d020d42c0297e63da8af22919568e3e049d2938
dspain <dmspain@yahoo.com>
2017-10-15 21:30:02 -0700

    longest line using streams - using built in File.stream!

--- Chapter10/longest_line_stream.exs
+++ Chapter10/longest_line_stream.exs
@@ -1,3 +1,5 @@
-IO.puts File.open!("/usr/share/dict/words")
-        |> IO.stream(:line)
-        |> Enum.max_by(&String.length/1)
+# IO.puts File.open!("/usr/share/dict/words")
+#         |> IO.stream(:line)
+#         |> Enum.max_by(&String.length/1)
+
+IO.puts File.stream!("/usr/share/dict/words") |> Enum.max_by(&String.length/1)
