8eba3966958ac0d5268129ae53113a4368a10317
dspain <dmspain@yahoo.com>
2017-10-17 21:47:37 -0700

    parse a string into a number

---
+++ Chapter11/parse.exs
@@ -0,0 +1,20 @@
+defmodule Parse do
+
+  def number([ ?- | tail ]), do: _number_digits(tail, 0) * -1
+  def number([ ?+ | tail ]), do: _number_digits(tail, 0)
+  def number(str),           do: _number_digits(str,  0)
+
+  defp _number_digits([], value), do: value
+  defp _number_digits([ digit | tail ], value)
+  when digit in '0123456789' do
+    IO.puts '--------'
+    IO.inspect tail
+    IO.inspect value
+    IO.inspect digit - ?0
+    IO.puts '--------'
+    _number_digits(tail, value*10 + digit - ?0)
+  end
+  defp _number_digits([ non_digit | _ ], _) do
+    raise "Invalid digit '#{[non_digit]}'"
+  end
+end
