fa9e0c343c525f8e46744137f7a8b9dadd51ab40
dspain <dmspain@yahoo.com>
2017-10-21 21:30:58 -0700

    show type of error in message too

--- Chapter12/ok_data.exs
+++ Chapter12/ok_data.exs
@@ -1,8 +1,8 @@
 defmodule Ok do
   def ok!(param) do
     case param do
-      {:ok, data} -> data
-      {_,   data} -> raise "Failure - here's some info: #{data}"
+      {:ok,  data} -> data
+      {type, data} -> raise "Failure - here's some info: #{type} -- #{data}"
     end
   end
 end
