475e52e82843edbeb7aa7e34feaa3bb688bff1a0
dspain <dmspain@yahoo.com>
2017-10-22 00:01:57 -0700

    fix typo

--- Chapter13/issues/lib/issues/github_issues.ex
+++ Chapter13/issues/lib/issues/github_issues.ex
@@ -1,7 +1,7 @@
 defmodule Issues.GithubIssues do
   @user_agent [ {"User-agent", "Elixir dave@pragprog.com"} ]
   # use a module attribute to fetch the value at compile time
-  @github_url Application.get_env(:ussues, :github_url)
+  @github_url Application.get_env(:issues, :github_url)
 
   def fetch(user, project) do
     issues_url(user, project)
