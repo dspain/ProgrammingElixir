2e465df89677a3ebdb1c28f44d33cb77f42a57b9
dspain <dmspain@yahoo.com>
2017-10-22 22:16:21 -0700

    add some more property tests - 'average' fails on empty list

--- Chapter14/pbt/test/stats_property_test.exs
+++ Chapter14/pbt/test/stats_property_test.exs
@@ -3,11 +3,20 @@ defmodule StatsPropertyTest do
   use ExCheck
 
   describe "Stats on lists of ints" do
+    property "count not negative" do
+      for_all l in list(int), do: Stats.count(l) >= 0
+    end
+
     property "single element lists are their own sum" do
       for_all number in real do
         Stats.sum([number]) == number
       end
     end
 
+    property "sum equals average times count" do
+      for_all l in list(int) do
+        abs(Stats.sum(l) - Stats.count(l) * Stats.average(l)) < 1.0e-6
+      end
+    end
   end
 end
