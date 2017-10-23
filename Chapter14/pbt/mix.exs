f1fcfa68179ef9a43ddfdfc2e40467211d6d3e80
dspain <dmspain@yahoo.com>
2017-10-22 21:40:35 -0700

    add ExCheck (property testing library)

--- Chapter14/pbt/mix.exs
+++ Chapter14/pbt/mix.exs
@@ -21,8 +21,8 @@ defmodule Pbt.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      { :triq, github: "krestenkrab/triq", only: :test },
+      { :excheck,  "~> 0.4.0", only: :test },
     ]
   end
 end
