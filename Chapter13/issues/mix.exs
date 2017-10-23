7f96292f40c1c4e94d990d7df69db95ef8eb6354
dspain <dmspain@yahoo.com>
2017-10-22 22:23:01 -0700

    add ExCoveralls test coverage tool

--- Chapter13/issues/mix.exs
+++ Chapter13/issues/mix.exs
@@ -10,7 +10,14 @@ defmodule Issues.Mixfile do
       start_permanent: Mix.env == :prod,
       deps: deps(),
       name: "Programming Elixir 1.3",
-      source_url: "https://github.com/dspain/ProgrammingElixir"
+      source_url: "https://github.com/dspain/ProgrammingElixir",
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        "coveralls": :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
     ]
   end
 
@@ -27,7 +34,8 @@ defmodule Issues.Mixfile do
       {:httpoison, "~> 0.9"},
       {:poison,    "~> 2.2"},
       {:ex_doc,    "~> 0.12"},
-      {:earmark,   "~> 1.0", override: true}
+      {:earmark,   "~> 1.0", override: true},
+      {:excoveralls, "~> 0.5", only: :test}
     ]
   end
 
