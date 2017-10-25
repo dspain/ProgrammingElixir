3189d548abe88aa6645a56a46211c6ed4e436302
dspain <dmspain@yahoo.com>
2017-10-24 21:21:11 -0700

    remove random sleeps

--- Chapter15/pmap.exs
+++ Chapter15/pmap.exs
@@ -3,13 +3,10 @@ defmodule Parallel do
     me = self
     collection
     |> Enum.map(fn (elem) ->
-         spawn_link fn -> (
-          :timer.sleep Enum.random(0..200)
-          send me, { self, fun.(elem) })
-        end
+         spawn_link fn -> ( send me, { self, fun.(elem) }) end
        end)
     |> Enum.map(fn (pid) ->
-         receive do { _pid, result } -> result end
+         receive do { ^pid, result } -> result end
        end)
   end
 end
