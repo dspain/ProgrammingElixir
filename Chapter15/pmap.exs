6ebdabd4d7773f12bd837c8d82da9e10a261911c
dspain <dmspain@yahoo.com>
2017-10-24 21:20:20 -0700

    WorkingWithMultipleProcesses-7 add a sleep to show how results can come back out of order if you don't use '^pid'

--- Chapter15/pmap.exs
+++ Chapter15/pmap.exs
@@ -3,10 +3,13 @@ defmodule Parallel do
     me = self
     collection
     |> Enum.map(fn (elem) ->
-         spawn_link fn -> (send me, { self, fun.(elem) }) end
+         spawn_link fn -> (
+          :timer.sleep Enum.random(0..200)
+          send me, { self, fun.(elem) })
+        end
        end)
     |> Enum.map(fn (pid) ->
-         receive do { ^pid, result } -> result end
+         receive do { _pid, result } -> result end
        end)
   end
 end
