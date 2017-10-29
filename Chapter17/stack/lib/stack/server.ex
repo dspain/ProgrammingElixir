3b2d6cfa725ba3721dc85dce9dbc7a9f998ab99b
dspain <dmspain@yahoo.com>
2017-10-29 15:50:10 -0700

    OTP-Servers-2 add push to Stack.Server

--- Chapter17/stack/lib/stack/server.ex
+++ Chapter17/stack/lib/stack/server.ex
@@ -4,4 +4,8 @@ defmodule Stack.Server do
   def handle_call(:pop, _from, [head | tail]) do
     { :reply, head, tail }
   end
+
+  def handle_cast({:push, val}, list) do
+    { :noreply, [val|list]}
+  end
 end
