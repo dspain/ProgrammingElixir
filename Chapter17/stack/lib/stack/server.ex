2a12c029f84e803b609f1baba03047cc58fcd92f
dspain <dmspain@yahoo.com>
2017-10-29 16:43:13 -0700

    OTP-Servers-4 create better API for Stack.Server

--- Chapter17/stack/lib/stack/server.ex
+++ Chapter17/stack/lib/stack/server.ex
@@ -1,6 +1,20 @@
 defmodule Stack.Server do
   use GenServer
 
+  ## APIT
+  def start_link(list) do
+    GenServer.start_link(__MODULE__, list, name:  __MODULE__)
+  end
+
+  def pop do
+    GenServer.call(__MODULE__, :pop)
+  end
+
+  def push(val) do
+    GenServer.cast(__MODULE__, {:push, val})
+  end
+
+  # GenServer calls
   def handle_call(:pop, _from, [head | tail]) do
     { :reply, head, tail }
   end
