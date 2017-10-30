d4eed996ed53c8f4f2b78f80e84f0da2cbf668c1
dspain <dmspain@yahoo.com>
2017-10-29 21:16:33 -0700

    fix handle_call & handle_cast functions

--- Chapter18/sequence/lib/sequence/server.ex
+++ Chapter18/sequence/lib/sequence/server.ex
@@ -23,11 +23,11 @@ defmodule Sequence.Server do
     current_number = Sequence.Stash.get_value stash_pid
     { :ok, {current_number, stash_pid} }
   end
-  def handle_call(:next_number, _from, current_number) do
-    { :reply, current_number, current_number+1 }
+  def handle_call(:next_number, _from, {current_number, stash_pid}) do
+    { :reply, current_number, {current_number+1, stash_pid} }
   end
-  def handle_cast({:increment_number, delta }, current_number) do
-    { :noreply, current_number + delta}
+  def handle_cast({:increment_number, delta }, {current_number, stash_pid}) do
+    { :noreply, {current_number + delta, stash_pid} }
   end
   def format_status(_reason, [_pdict, state]) do
     [data: [{'State', "My current state is '#{inspect state}', and I'm happy!"}]]
