9989efacdf7912448077197f3c20d082f6c3a04d
dspain <dmspain@yahoo.com>
2017-11-03 22:14:14 -0700

    implement 'member?' function

--- Chapter23/bitmap_enumerable.exs
+++ Chapter23/bitmap_enumerable.exs
@@ -3,7 +3,14 @@ defimpl Enumerable, for: Bitmap do
   def count(%Bitmap{value: value}) do
     { :ok, trunc(log(abs(value))/log(2)) + 1 }
   end
+
+  def member?(value, bit_number) do
+    { :ok, 0 <= bit_number && bit_number < Enum.count(value) }
+  end
 end
 
 fifty = %Bitmap{value: 50}
-IO.puts Enum.count fifty     # => 6
+IO.puts Enum.count fifty        # => 6
+
+IO.puts Enum.member? fifty, 4   # => true
+IO.puts Enum.member? fifty, 6   # => false
