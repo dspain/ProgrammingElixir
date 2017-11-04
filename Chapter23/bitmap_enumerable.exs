c02f772952dce94d368e11fdc435a8ffa56eb236
dspain <dmspain@yahoo.com>
2017-11-03 22:29:22 -0700

    implement 'reduce' function

--- Chapter23/bitmap_enumerable.exs
+++ Chapter23/bitmap_enumerable.exs
@@ -7,6 +7,20 @@ defimpl Enumerable, for: Bitmap do
   def member?(value, bit_number) do
     { :ok, 0 <= bit_number && bit_number < Enum.count(value) }
   end
+
+  def reduce(bitmap, {:cont, acc}, fun) do
+    bit_count = Enum.count(bitmap)
+    _reduce({bitmap, bit_count}, { :cont, acc }, fun)
+  end
+
+  defp _reduce({_bitmap, -1}, { :cont, acc}, _fun), do: { :done, acc }
+  defp _reduce({bitmap, bit_number}, { :cont, acc }, fun) do
+    with bit = Bitmap.fetch_bit(bitmap, bit_number),
+    do: _reduce({bitmap, bit_number-1}, fun.(bit,acc), fun)
+  end
+  defp _reduce({_bitmap, _bit_number}, { :halt, acc }, _fun), do: { :halted, acc }
+  defp _reduce({bitmap, bit_number}, { :suspend, acc }, fun),
+  do: { :suspended, acc, &_reduce({bitmap, bit_number}, &1, fun), fun }
 end
 
 fifty = %Bitmap{value: 50}
@@ -14,3 +28,6 @@ IO.puts Enum.count fifty        # => 6
 
 IO.puts Enum.member? fifty, 4   # => true
 IO.puts Enum.member? fifty, 6   # => false
+
+IO.inspect Enum.reverse fifty   # => [0, 1, 0, 0, 1, 1, 0]
+IO.inspect Enum.join fifty, ":" # => "0:1:1:0:0:1:0"
