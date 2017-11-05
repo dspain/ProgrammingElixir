e5f592ea8bc31f8d587650cbc5e9511bad8a1175
dspain <dmspain@yahoo.com>
2017-11-04 22:28:05 -0700

    comment out line_sigil tests

--- Chapter24/eval/apps/line_sigil/test/line_sigil_test.exs
+++ Chapter24/eval/apps/line_sigil/test/line_sigil_test.exs
@@ -1,8 +1,8 @@
 defmodule LineSigilTest do
   use ExUnit.Case
-  doctest LineSigil
-
-  test "greets the world" do
-    assert LineSigil.hello() == :world
-  end
+  # doctest LineSigil
+  #
+  # test "greets the world" do
+  #   assert LineSigil.hello() == :world
+  # end
 end
